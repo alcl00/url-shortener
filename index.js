2289e4a972c58545446bc4a712abb894df876a04
Alex Levinson <61521851+alcl00@users.noreply.github.com>
2023-04-11 13:10:41 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let mongoose=require('mongoose');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+  original_url: {type: String, required: true},
+  short_url: { type: Number, required: true }
+});
+
+let Url = mongoose.model("Urls", urlSchema);
+
 app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +33,49 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const originalUrls = [];
+const shortUrls = [];
+
+app.post('/api/shorturl', (req, res, done) => {
+  const url = req.body.url;
+
+  const foundIndex = originalUrls.indexOf(url);
+  
+  const urlRegex = /^(https?:\/\/)(www.)?[A-Za-z]+.[A-Za-z]+/;
+  
+  if(url.match(urlRegex)) {
+    if(foundIndex < 0)
+    {
+      originalUrls.push(url);
+      shortUrls.push(shortUrls.length);
+
+      return res.json({
+        original_url: url,
+        short_url: shortUrls.length - 1
+        });
+      
+    }
+    return res.json({
+        original_url: url,
+        short_url: shortUrls[foundIndex]
+      });
+  }
+  else {
+    return res.json({error: 'invalid url'})
+  }
+});
+
+app.get('/api/shorturl/:shorturl', (req,res) => {
+  const shortUrl = parseInt(req.params.shorturl);
+  const foundIndex = shortUrls.indexOf(shortUrl);
+
+  if(foundIndex < 0){
+    return res.json({error: 'invalid url'});
+  }
+
+  res.redirect(originalUrls[foundIndex]);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
